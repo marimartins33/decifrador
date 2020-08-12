4d98a873ece62ec914c97ab54bdc8b0e77b7f605
marimartins33 <marianemartins1997@gmail.com>
2020-08-12 04:14:29 -0300

    75% certo

--- src/main.c
+++ src/main.c
@@ -7,12 +7,12 @@ char buffer_saida[TAM_BUFFER];
 
 const char senha[] = "SENHASECRETA";
 
-char soma_modulo_alfabeto(char a, char b) {
+char decifrar(char a, char b) {
   char a0, b0, c0;
   if (a>='A' && a<='Z') {
     a0 = a-'A';
     b0 = b-'A';
-    c0 = 'A'+(a0-b0)%('Z'-'A'+1);
+    c0 = 'A'+(a0-b0+'Z'-'A'+1)%('Z'-'A'+1);
     return c0;
   } else return a;
 }
@@ -27,7 +27,7 @@ int main() {
 
   for (i=0; (i<TAM_BUFFER) && (buffer_entrada[i]!='\0') && (buffer_entrada[i]!='\n'); i++) {
 
-    buffer_saida[i] = soma_modulo_alfabeto(buffer_entrada[i], senha[j]);
+    buffer_saida[i] = decifrar(buffer_entrada[i], senha[j]);
     j++;
     if (senha[j]=='\0') j=0;
   }
@@ -37,27 +37,4 @@ int main() {
   /* Escrever saida na tela */
   printf("%s\n", buffer_saida);
 
-  return 0;
-}* Tarefa de decifrar uma cifra
- *
- * Dica: veja em referencia/cifrador.c o codigo-fonte do programa
- * que gerou as cifras que voce precisa decifrar!
- */
-
-#include <stdio.h>
-
-#define TAM_BUFFER 50
-char buffer_entrada[TAM_BUFFER];
-char buffer_saida[TAM_BUFFER];
-
-int main() {
-  /* Leitura da entrada */
-  fgets(buffer_entrada, TAM_BUFFER, stdin);
-
-
-
-  /* Escrever saida na tela */
-  printf("%s\n", buffer_saida);
-
-  return 0;
-}
+  return 0;}
